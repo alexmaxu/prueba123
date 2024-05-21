f884c983d04683d09944f76168e1a5af3e54896d
alex <12alexmaxu@gmail.com>
2024-05-21 11:06:21 +0200

    Initial Commit

---
+++ prueba123/prueba123App.swift
@@ -0,0 +1,17 @@
+//
+//  prueba123App.swift
+//  prueba123
+//
+//  Created by Alex  on 21/5/24.
+//
+
+import SwiftUI
+
+@main
+struct prueba123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
